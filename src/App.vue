17f0df1649ee780ba52875cf85be8ed24010f09e
sasa-bonta <abonta@pentalog.com>
2023-03-20 17:57:57 +0200

    main page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-container>
+    <v-row
+        justify="center"
+    >
+      <img src="zebra.png" alt="zebra.jpg">
+    </v-row>
+    <v-row
+        justify="center"
+    >
+      <h1 id="system-name">Zebra Search</h1>
+    </v-row>
+    <v-row
+        justify="center"
+        align="center"
+    >
+      <v-col cols="3"></v-col>
+      <v-col cols="6">
+        <v-autocomplete
+            clearable
+            label="What are you looking for?"
+            :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+            variant="solo"
+        ></v-autocomplete>
+      </v-col>
+      <v-col cols="3"></v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
@@ -22,5 +44,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  background: url("/public/background.jpg");
+  background-size: cover;
+}
+
+img {
+  height: 45vh;
+}
+
+#system-name {
+  background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);
+  -webkit-background-clip: text;
+  color: transparent;
 }
 </style>
