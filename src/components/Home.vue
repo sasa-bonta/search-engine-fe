16f5a9bc7557a5b78516d32dbb9d8d2efa58addd
sasa-bonta <abonta@pentalog.com>
2023-03-20 22:23:37 +0200

    home, search components

---
+++ src/components/Home.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-container>
+    <v-row
+        justify="center"
+    >
+      <img src="zebra.png" alt="zebra.jpg">
+    </v-row>
+    <v-row
+        justify="center"
+    >
+      <h1 id="system-name">Zebra Search</h1>
+    </v-row>
+    <v-row
+        justify="center"
+        align="center"
+    >
+      <v-col cols="3"></v-col>
+      <v-col cols="6">
+        <!--        <v-autocomplete-->
+        <!--            clearable-->
+        <!--            label="What are you looking for?"-->
+        <!--            :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"-->
+        <!--            no-data-text="No suggestions"-->
+        <!--            :no-filter="true"-->
+        <!--            variant="solo"-->
+        <!--        ></v-autocomplete>-->
+        <v-sheet>
+          <v-text-field
+              v-model="search"
+              label="What are you looking for?"
+              hide-details
+          >
+            <template #append>
+              <v-btn variant="plain" prepend-icon="mdi-vuetify" class="mb-3 mr-3"> Go</v-btn>
+            </template>
+          </v-text-field>
+        </v-sheet>
+      </v-col>
+      <v-col cols="3"></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  name: 'HomeComponent',
+
+  setup() {
+    let search = ref('')
+
+    return {
+      search
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  background: url("/public/background.jpg");
+  background-size: cover;
+}
+
+img {
+  height: 45vh;
+}
+
+#system-name {
+  background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);
+  -webkit-background-clip: text;
+  color: transparent;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import {createRouter, createWebHashHistory} from 'vue-router'
+import Home from "@/components/Home";
+import Search from "@/components/Search";
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/search',
+        name: 'search',
+        component: Search
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
